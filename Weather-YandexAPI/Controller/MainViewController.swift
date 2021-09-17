f36920dc735fa232dbb478dc4b62ee119309e48d
Ivan <izzyumkin@gmail.com>
2021-09-17 15:52:32 +0500

    Initial commit

---
+++ Weather-YandexAPI/Controller/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Weather-YandexAPI
+//
+//  Created by Иван Изюмкин on 17.09.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
